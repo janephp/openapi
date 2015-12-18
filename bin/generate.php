0b99ad6b7cdcdb1df27be6c7aea0976aa531f102
Joel Wurtz <jwurtz@jolicode.com>
2015-12-18 17:18:59 +0100

    Use jane binary for generation

--- bin/generate.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once __DIR__.'/../vendor/autoload.php';
-
-$jane = \Joli\Jane\Jane::build();
-$jane->generate(__DIR__ . '/../swagger-spec.json', 'Swagger', 'Joli\\Jane\\Swagger', __DIR__ . '/../generated');
