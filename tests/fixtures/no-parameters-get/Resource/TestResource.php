123e647bf84e90d2362093f7e9324868b901bf05
Joel Wurtz <jwurtz@jolicode.com>
2015-12-21 11:50:03 +0100

    Fix tests with updated generation

--- tests/fixtures/no-parameters-get/Resource/TestResource.php
+++ tests/fixtures/no-parameters-get/Resource/TestResource.php
@@ -7,8 +7,8 @@ use Joli\Jane\Swagger\Client\Resource;
 class TestResource extends Resource
 {
     /**
-     * @param array $parameters
-     * @param string $fetch
+     * @param array  $parameters List of parameters
+     * @param string $fetch      Fetch mode (object or response)
      *
      * @return \Psr\Http\Message\ResponseInterface
      */
@@ -18,6 +18,9 @@ class TestResource extends Resource
         $url = sprintf('/test?%s', $queryParam->buildQueryString($parameters));
         $request = $this->messageFactory->createRequest('GET', $url, $queryParam->buildHeaders($parameters), null);
         $response = $this->httpClient->sendRequest($request);
+        if (self::FETCH_RESPONSE == $fetch) {
+            return $response;
+        }
         return $response;
     }
 }
