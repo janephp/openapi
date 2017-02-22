0476d2c1327b47085824d34c4b5d67513eb00616
Emilien Mantel <emilien.mantel@na-work.com>
2017-02-22 14:29:45 +0100

    Manage special paths:
    - cleanup special chars (accepted by swagger)
    - fix classname generated like '/test'

---
+++ tests/fixtures/model-in-response/expected/Normalizer/TestIdResponse200Normalizer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Joli\Jane\OpenApi\Tests\Expected\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\SerializerAwareNormalizer;
+
+class TestIdResponse200Normalizer extends SerializerAwareNormalizer implements DenormalizerInterface, NormalizerInterface
+{
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        if ($type !== 'Joli\\Jane\\OpenApi\\Tests\\Expected\\Model\\TestIdResponse200') {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        if ($data instanceof \Joli\Jane\OpenApi\Tests\Expected\Model\TestIdResponse200) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        $object = new \Joli\Jane\OpenApi\Tests\Expected\Model\TestIdResponse200();
+        if (property_exists($data, 'id')) {
+            $object->setId($data->{'id'});
+        }
+
+        return $object;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $data = new \stdClass();
+        if (null !== $object->getId()) {
+            $data->{'id'} = $object->getId();
+        }
+
+        return $data;
+    }
+}
